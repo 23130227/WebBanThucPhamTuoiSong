0663577d3f3c02077618fa477635b11528a4f1c5
Lưu Quốc Pháp <23130227@st.hcmuaf.edu.vn>
2025-11-24 19:11:45 +0700

    Luu Quoc Phap

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def home(request):
+    context = {}
+    return render(request, 'app/home.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'app/cart.html', context)
