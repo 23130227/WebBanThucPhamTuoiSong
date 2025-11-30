29cecbc8e76efb81f1f68f371035024ce60919f5
Lưu Quốc Pháp <23130227@st.hcmuaf.edu.vn>
2025-12-01 00:40:40 +0700

    Them thao tac cac trang

---
+++ products/context_processors.py
@@ -0,0 +1,5 @@
+from .models import *
+
+def category_list(request):
+    categories = Category.objects.all()
+    return {'category_list': categories}
--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('shop/product-single/', views.product_single, name='product-single'),
+    path("<slug:category_slug>/<slug:product_slug>/", views.product_single, name="product-single"),
     path('shop/', views.shop, name='shop'),
     path('search-results/', views.search_results, name='search-results'),
 ]
