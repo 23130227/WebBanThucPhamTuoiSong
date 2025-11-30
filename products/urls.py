83028d9ecd334c477a48e17f1672ac216ae2f680
Lưu Quốc Pháp <23130227@st.hcmuaf.edu.vn>
2025-11-30 20:10:32 +0700

    Chinh sua mot so trang

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product-single/', views.product_single, name='product-single'),
+    path('shop/product-single/', views.product_single, name='product-single'),
     path('shop/', views.shop, name='shop'),
     path('search-results/', views.search_results, name='search-results'),
 ]
