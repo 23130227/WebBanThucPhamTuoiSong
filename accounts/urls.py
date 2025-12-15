f3791fb55a83e14efa2f3c29d75c7b7a0b357789
Lưu Quốc Pháp <23130227@st.hcmuaf.edu.vn>
2025-12-15 10:47:30 +0700

    Sửa logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('dang-ky/', views.register_view, name='register'),
     path('dang-nhap/', views.login_view, name='login'),
-    path('dang-xuat/', views.logout_view, name='logout'),
+    path('dang-xuat/', LogoutView.as_view(next_page='login'), name='logout'),
     path('quen-mat-khau/', views.forgot_password_view, name='forgot-password'),
     path('thong-tin-ca-nhan/', views.profile_view, name='profile'),
     path('doi-mat-khau/', views.password_change_view, name='password-change'),
